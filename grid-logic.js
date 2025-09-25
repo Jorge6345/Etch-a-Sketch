c11c74db8537a4c98b15d3c6e29a5c4dd8e98cd2
Jorge <129089686+Jorge6345@users.noreply.github.com>
2025-09-25 02:34:13 -0400

    V1.0 of Etch-a-Sketch project.
    Still want to add the option for user to select painting color.

---
+++ grid-logic.js
@@ -0,0 +1,52 @@
+const gridContainer = document.getElementById("grid-container");
+const setGridAmountButton = document.getElementById("set-grid-amount");
+const resetGridButton = document.getElementById("reset-button");
+const displayGridSize = document.getElementById("display-grid-size");
+
+function makeGrid (gridSize) {
+    displayGridSize.textContent = "Grid Size: " + gridSize + "x" + gridSize;
+
+    gridContainer.style.setProperty('--n', gridSize);
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridBox = document.createElement("div");
+        gridBox.classList.add("grid");
+
+        gridContainer.appendChild(gridBox);
+    }
+}
+
+function setBrushColor (brushColor) {
+    const allGrids = document.querySelectorAll(".grid");
+    allGrids.forEach(grid => {
+        grid.addEventListener("mouseenter", () => {
+            grid.style.backgroundColor = brushColor;
+        });
+    });
+}
+
+function resetGridColor () {
+    const allGrids = document.querySelectorAll(".grid");
+    allGrids.forEach(grid => {
+        grid.style.backgroundColor = "black";
+    });
+}
+
+makeGrid(16);
+setBrushColor("blue");
+
+setGridAmountButton.addEventListener("click", () => {
+    const deleteAllGrids = document.querySelectorAll(".grid");
+    deleteAllGrids.forEach(grid => {
+        grid.classList.remove("grid");
+    })
+
+    let userGridSize = prompt("Please select grid size");
+
+    makeGrid(userGridSize);
+    setBrushColor("blue");
+})
+
+resetGridButton.addEventListener("click", () => {
+    resetGridColor();
+})
